0dce1aa2fadf7ec02a1069af4f62de199fd826fe
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-01-10 00:25:42 +0530

    DSA-DAY-01

---
+++ DAY-01/Demo.java
@@ -0,0 +1,36 @@
+class Demo {
+    public static void main(String[] args) {
+
+        // 🔢 Integer type
+        int age = 22;
+        System.out.println("Age = " + age);
+
+        // 🔢 Floating type
+        float percentage = 85.5f;
+        double salary = 45000.75;
+        System.out.println("Percentage = " + percentage);
+        System.out.println("Salary = " + salary);
+
+        // 🔤 Character type
+        char grade = 'A';
+        System.out.println("Grade = " + grade);
+
+        // ✅ Boolean type
+        boolean isPassed = true;
+        System.out.println("Passed = " + isPassed);
+
+        // 📝 String type
+        String name = "Vaibhaw";
+        System.out.println("Name = " + name);
+
+        // 🔁 Updating variable value
+        age = 23;
+        System.out.println("Updated Age = " + age);
+
+        // ➕ Simple calculation
+        int a = 10;
+        int b = 20;
+        int sum = a + b;
+        System.out.println("Sum = " + sum);
+    }
+}
