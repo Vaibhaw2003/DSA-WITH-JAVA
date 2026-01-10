80f402b5720fde05b75c0eeb35c59ee6fb6c07cf
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-01-10 16:28:55 +0530

    DSA-DAY-03

--- DAY-02/Demo.java
+++ DAY-02/Demo.java
@@ -1,11 +1,40 @@
 //----------DAY-02/Demo.java----------[ LOOPS ]----------
 
-class Demo {
-    public static void main(String[] args){
-        System.out.println("For Loop:");
+// class Demo {
+//     public static void main(String[] args){
+//         System.out.println("For Loop:");
 
-        for( int i = 1; i <= 5; i++ ) {
-            System.out.println("Iteration: " + i);
-        }
-    }
-}
+//         for( int i = 1; i <= 5; i++ ) {
+//             System.out.println("Iteration: " + i);
+//         }
+//     }
+// }
+
+
+//------WHILE LOOP VERSION OF THE SAME CODE------
+
+// class Demo {
+//     public static void main(String[] args){
+//         System.out.println("While Loop:");
+
+//         int i = 1;
+//         while( i <= 5 ) {
+//             System.out.println("Iteration: " + i);
+//             i++;
+//         }
+//     }
+// }
+
+//------DO WHILE LOOP VERSION OF THE SAME CODE------
+
+// class Demo {
+//     public static void main(String[] args){
+//         System.out.println("Do While Loop:");
+
+//         int i = 1;
+//         do {
+//             System.out.println("Iteration: " + i);
+//             i++;
+//         } while( i <= 5 );
+//     }
+// }
--- DAY-03/Demo.java
+++ DAY-03/Demo.java
@@ -1,29 +1,63 @@
 //----------DAY-03/Demo.java----------[ ARRAYS, 1D,2D ]----------
 
-class Demo {
-    public static void main(String[] args){
-        System.out.println("1D Array:");
+// class Demo {
+//     public static void main(String[] args){
+//         System.out.println("1D Array:");
 
-        int[] arr1D = {10, 20, 30, 40, 50};
+//         int[] arr1D = {10, 20, 30, 40, 50};
 
-        for(int i = 0; i < arr1D.length; i++) {
-            System.out.println("Element at index " + i + ": " + arr1D[i]);
-        }
+//         for(int i = 0; i < arr1D.length; i++) {
+//             System.out.println("Element at index " + i + ": " + arr1D[i]);
+//         }
 
-        System.out.println("\n2D Array:");
+//         System.out.println("\n2D Array:");
 
-        int[][] arr2D = {
-            {1, 2, 3},
-            {4, 5, 6},
-            {7, 8, 9}
-        };
+//         int[][] arr2D = {
+//             {1, 2, 3},
+//             {4, 5, 6},
+//             {7, 8, 9}
+//         };
 
-        for(int i = 0; i < arr2D.length; i++) {
-            for(int j = 0; j < arr2D[i].length; j++) {
-                System.out.print(arr2D[i][j] + " ");
-            }
-            System.out.println();
-        }
-    }
+//         for(int i = 0; i < arr2D.length; i++) {
+//             for(int j = 0; j < arr2D[i].length; j++) {
+//                 System.out.print(arr2D[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
     
-}
+// }
+
+
+//----------OR----------
+
+// class Demo {
+//     public static void main(String[] args){
+//         System.out.println("Storing marks of 5 subjects using 1D Array:");
+//     int[] marks = new int[5];
+//     marks[0] = 76;
+//     marks[1] = 85;
+//     marks[2] = 69;
+//     marks[3] = 90;
+//     marks[4] = 88;
+
+//     //System.out.println(marks[0] + ", " + marks[1] + ", " + marks[2] + ", " + marks[3] + ", " + marks[4]);
+//     for(int i = 0; i<5; i++){
+//         System.out.println("marks of subjects " + marks[i]);
+//           }
+//     }
+// }
+
+//----------OR----------    
+
+// public class Demo {
+//     public static void main(String[] args) {
+//         int age[] = {21, 22, 23, 24, 25};
+
+//         // System.out.println("Ages of 5 students are:" + age[0] + ", " + age[1] + ", " + age[2] + ", " + age[3] + ", " + age[4]);
+
+//         for( int i =0; i<age.length; i++){
+//             System.out.println("Ages of students are: " + age[i]);
+//         }
+//     }  
+// }
