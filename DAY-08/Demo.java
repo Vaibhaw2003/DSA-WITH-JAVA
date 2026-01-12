824e8f1f07866f6579bf87e7bf9159965da34c17
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-01-12 13:19:03 +0530

    OOPS

--- DAY-08/Demo.java
+++ DAY-08/Demo.java
@@ -1,11 +1,38 @@
 //----------DAY-08/Demo.java----------[ OOPS, CLASS, OBJECT, CONSTRUCTOR ]----------
 
+// public class Demo {
+//     // Class attributes
+//     String name;
+//     int age;
+
+//     // Constructor
+//     public Demo(String name, int age) {
+//         this.name = name;
+//         this.age = age;
+//     }
+
+//     // Method to display details
+//     public void displayInfo() {
+//         System.out.println("Name: " + name);
+//         System.out.println("Age: " + age);
+//     }
+
+//     public static void main(String[] args) {
+//         // Creating an object of the Demo class
+//         Demo person = new Demo("Alice", 30);
+
+//         // Calling the method to display details
+//         person.displayInfo();
+//     }
+// }
+
+//------- [ PARAMETERIZED CONSTRUCTOR ] -------
 public class Demo {
     // Class attributes
     String name;
     int age;
 
-    // Constructor
+    // Parameterized Constructor
     public Demo(String name, int age) {
         this.name = name;
         this.age = age;
@@ -18,11 +45,10 @@ public class Demo {
     }
 
     public static void main(String[] args) {
-        // Creating an object of the Demo class
-        Demo person = new Demo("Alice", 30);
+        // Creating an object of the Demo class using parameterized constructor
+        Demo person = new Demo("Bob", 25);
 
         // Calling the method to display details
         person.displayInfo();
     }
-    
 }
