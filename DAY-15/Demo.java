cb1df4a6a3d042164f20ad023a51447f47a4e9ef
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-01-13 11:37:53 +0530

    RECURSION

--- DAY-15/Demo.java
+++ DAY-15/Demo.java
@@ -19,17 +19,35 @@
 
 //------- [ TO PRINT NUMBERS FROM 1 TO 5 USING RECURSION ] -------
 
-class Demo {
-    public static void PrintNum (int n){
-        if(n==6){
-            return;
-        }
-        System.out.println(n);
-        PrintNum(n+1);
-    }
+// class Demo {
+//     public static void PrintNum (int n){
+//         if(n==6){
+//             return;
+//         }
+//         System.out.println(n);
+//         PrintNum(n+1);
+//     }
 
-    public static void main(String[] args) {
-        int n = 1;
-        PrintNum(n);
-    }
-}
+//     public static void main(String[] args) {
+//         int n = 1;
+//         PrintNum(n);
+//     }
+// }
+
+
+//------- [ TO PRINT SUM OF FIRST N NATURAL NUMBERS USING RECURSION ] -------
+
+// class Demo {
+//     public static int SumNum (int n){
+//         if(n==1){
+//             return 1;
+//         }
+//         return n + SumNum(n-1);
+//     }
+
+//     public static void main(String[] args) {
+//         int n = 6;
+//         int sum = SumNum(n);
+//         System.out.println("Sum of first " + n + " natural numbers is: " + sum);
+//     }
+// }
