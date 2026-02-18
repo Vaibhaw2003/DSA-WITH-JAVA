bd3d13d92c2a56c05cf586e73410566da0cd54df
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-02-18 10:46:07 +0530

    DSA CLASS-01

--- DAY-01/Demo.java
+++ DAY-01/Demo.java
@@ -36,3 +36,53 @@
 //         System.out.println("Sum = " + sum);
 //     }
 // }
+
+//------ 
+// import java.util.*;
+// class Demo{
+//     public static void main(String[] args) {
+//         Vector <Integer> v = new Vector<>();
+//         v.add(33);
+//         v.add(35);
+//         v.add(55);
+//         v.set(0, 05);
+//         System.out.println(v);
+//         System.out.println(v.isEmpty());
+//         System.out.println(v.contains(v));
+
+//     } 
+//  }
+
+// class Demo {
+//     public static void main(String[] args) {
+//         int n=5;
+//         int i;
+//         for(i =1; i<=n; i++){
+//             System.out.println(i);
+//         }
+//     }
+// }
+
+
+import java.util.Scanner;;
+class Demo {
+    public static void main(String[] args){
+        int i;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number : ");
+        int n= sc.nextInt();
+        if(n>=0){
+            for(i= 1; i<=n; i++)
+                {
+                    System.out.println(" ");
+                }
+            for(int j =1; j<=n; j++){
+                System.out.println("*");
+            }
+            }
+        else {
+            System.out.println("erro");
+            }
+            sc.close();
+        }
+}
