5388c2f79d3c8073b43adce31c553d6473969161
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-08 18:27:33 +0530

    N-queen

--- RECURSION/Demo.java
+++ RECURSION/Demo.java
@@ -1,20 +1,20 @@
 //---------- FACTORIAL --------
 
-class Demo{
+// class Demo{
 
-    static int fact (int n){
-    if(n ==0)
-        return 1;
-    return n * fact(n-1);
+//     static int fact (int n){
+//     if(n ==0)
+//         return 1;
+//     return n * fact(n-1);
   
-}
+// }
 
-public static void main(String[] args) {
-    int n = 8;
-    System.out.println(fact(n));
-}
+// public static void main(String[] args) {
+//     int n = 8;
+//     System.out.println(fact(n));
+// }
 
-}
+// }
 
 
 
@@ -35,3 +35,23 @@ public static void main(String[] args) {
 //             print(n);
 //          }
 // }
+
+//------- N QUEEN PROBLEAM ----------
+
+class Demo{
+    static int n = 4;
+
+    static boolean isSafe(int board[][], int row , int col){
+        for(int i = 0; i<col; i++){
+            if (board[row][i]==1)
+                return false;
+
+        for( int j =row ,j =col; i>=0 && j>=0;i--,j-- )
+            if (board[i][j]==1) {
+                return false;
+                
+            }
+        }
+    }
+
+}
