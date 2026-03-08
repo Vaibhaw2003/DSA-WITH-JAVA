4a2903075d04640f02e6e33965958389dda88591
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-08 18:27:03 +0530

    STRINGS

--- ARRAY/Demo.java
+++ ARRAY/Demo.java
@@ -91,32 +91,58 @@
 // }
 
 //------ Two Pointer Technique -------------
-import java.*;
-public class Demo {
 
+// import java.*;
+// public class Demo {
+
+//     public static void main(String[] args) {
+
+//         int[] arr = {1,2,3,4,6};
+//         int target = 6;
+
+//         int left = 0;
+//         int right = arr.length-1;
+
+//         while(left < right){
+
+//             int sum = arr[left] + arr[right];
+
+//             if(sum == target){
+//                 System.out.println("Pair Found");
+//                 return;
+//             }
+
+//             if(sum < target)
+//                 left++;
+//             else
+//                 right--;
+//         }
+
+//         System.out.println("Pair Not Found");
+//     }
+// }
+
+
+//----- 02 ---- Two Pointer Technique ------------
+
+class Demo{
     public static void main(String[] args) {
-
-        int[] arr = {1,2,3,4,6};
-        int target = 6;
-
-        int left = 0;
-        int right = arr.length-1;
-
-        while(left < right){
-
-            int sum = arr[left] + arr[right];
-
-            if(sum == target){
-                System.out.println("Pair Found");
+        int arr[]={2,3,4,5,6,7};
+        int target =15;
+        int l=0;
+        int r=arr.length-1;
+        while (l<r) {
+            int sum =arr[r]+arr[l];
+            if (sum==target) {
+                System.out.println("pair found");
                 return;
             }
-
-            if(sum < target)
-                left++;
+            if(sum< target)
+                l++;
             else
-                right--;
+                r--;
         }
-
-        System.out.println("Pair Not Found");
+        System.out.println("pair not faund");
     }
 }
+
---
+++ STRINGS/Demo.java
@@ -0,0 +1,8 @@
+//------------  String Manipulation --------
+
+class Demo{
+    public static void main(String[] args) {
+        String s = "vaibhaw singh";
+        System.out.println(s.toUpperCase());
+    }
+}
