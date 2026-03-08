48e27cebf3cf2c71157aad916f428d76a5e37431
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-08 18:53:49 +0530

    REVERSE THE STRING

--- STRINGS/Demo.java
+++ STRINGS/Demo.java
@@ -1,8 +1,21 @@
 //------------  String Manipulation --------
 
-class Demo{
-    public static void main(String[] args) {
-        String s = "vaibhaw singh";
-        System.out.println(s.toUpperCase());
+// class Demo{
+//     public static void main(String[] args) {
+//         String s = "vaibhaw singh";
+//         System.out.println(s.toUpperCase());
+//     }
+// }
+
+//--------- REVERSE THE STRING ------
+class Demo {
+    public static void main(String[] args){
+        String s = "vaibhaw";
+         String rev = " ";
+
+        for(int i = s.length()-1; i>=0; i--){
+          rev  = rev + s.charAt(i);
+        }
+        System.out.println(rev);
     }
 }
