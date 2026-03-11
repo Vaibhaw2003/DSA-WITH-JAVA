6e3a8134c57402ef280b2fd591e32cd168cd3c7d
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-11 21:43:38 +0530

    PIRNT * PATTER

---
+++ PATTERN/Demo.java
@@ -0,0 +1,34 @@
+//---------- TO PRINT * PATTERN ---------
+
+// class Demo{
+//     public static void main(String[] args) {
+//         int i,j;
+//         int n=5;
+//         for(i=1; i<=n; i++)
+//             for(j =1; j<=i; j++){
+//         System.out.println("*");
+//         }
+
+//         System.out.println();
+//     }
+// }
+
+//-------- TO PRINT THE TRANGULAR * -------
+
+// import java.util.Scanner;
+// class Demo{
+//     public static void main(String[] args) {
+//         int i,j,n;
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the number : ");
+//         n = sc.nextInt();
+
+//         for(i=1; i<=n; i++){
+            
+//             for(j =1; j<=i; j++){
+//                 System.out.print("*" + " ");
+//             }System.out.println();
+//         }
+        
+//     }
+// }
