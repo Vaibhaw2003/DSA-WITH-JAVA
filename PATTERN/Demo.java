a871678af7fd3dc97a332a17cc1f039fcdb81492
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-12 22:02:07 +0530

    SQURE OF *

--- PATTERN/Demo.java
+++ PATTERN/Demo.java
@@ -32,3 +32,23 @@
         
 //     }
 // }
+
+//--------- TO PRINT SQUARE OR * PATTERN ----
+
+import java.util.Scanner;
+class Demo{
+    public static void main(String[] args) {
+        int i,j,n;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the number: ");
+        n = sc.nextInt();
+        
+        for(i = 1; i<=n ; i++){
+           
+            for(j =1; j<= n; j++){
+                System.out.print("*" + " ");
+            }System.out.println();
+        }
+
+    }
+}
