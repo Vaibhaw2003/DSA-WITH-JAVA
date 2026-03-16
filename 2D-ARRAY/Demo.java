33ca5bc6b81f9dbc7a3636eee09242d81aaa86b5
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-17 00:00:22 +0530

    TO PRINT THE 3*3 MATRIX

--- 2D-ARRAY/Demo.java
+++ 2D-ARRAY/Demo.java
@@ -1,37 +1,67 @@
 //----------- 2D ARRAY -----------
 
+// import java.util.Scanner;
+
+// class Demo{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the row : ");
+//         int row =sc.nextInt();
+//         System.out.println("enter the col");
+//         int col = sc.nextInt();
+
+//         int num[][] = new int[row][col];
+
+//         //input
+//         for(int i =0; i<row; i++){
+//             //colums
+//             for(int j =0; j< col; j++){
+//                 num[i][j] =sc.nextInt();
+//             }
+//         }
+
+//         //output
+//         System.out.println("the 2d array is : ");
+//          for(int i =0; i<row; i++){
+//             //colums
+//             for(int j =0; j< col; j++){
+//                 System.out.print(num[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+//------- TO PRINT THE 3*3 MATRIX ---------
+
 import java.util.Scanner;
 
 class Demo{
     public static void main(String[] args) {
+
         Scanner sc = new Scanner(System.in);
-        System.out.println("enter the row : ");
-        int row =sc.nextInt();
-        System.out.println("enter the col");
-        int col = sc.nextInt();
+         System.out.println("enter the row : ");
+         int row =sc.nextInt();
+         System.out.println("enter the col");
+         int col = sc.nextInt();
 
-        int num[][] = new int[row][col];
+         int num[][] = new int[row][col];
 
-        //input
-        for(int i =0; i<row; i++){
-            //colums
-            for(int j =0; j< col; j++){
+         // input 
+         for(int i =0; i<row; i++){
+            for(int j =0; j<col; j++ ){
                 num[i][j] =sc.nextInt();
             }
-        }
+         }
 
-        //output
-        System.out.println("the 2d array is : ");
-         for(int i =0; i<row; i++){
-            //colums
+         //output 
+         System.out.println("the output is here");
+         for(int i = 0; i< row; i++){
             for(int j =0; j< col; j++){
                 System.out.print(num[i][j] + " ");
             }
             System.out.println();
-        }
-
-
-
+         }
+        
     }
-
 }
