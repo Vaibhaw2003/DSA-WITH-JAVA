c4161cc02403b658c4e9f1d7bf04e0bfcd44dd97
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-16 23:52:33 +0530

    2D ARRAY

---
+++ 2D-ARRAY/Demo.java
@@ -0,0 +1,37 @@
+//----------- 2D ARRAY -----------
+
+import java.util.Scanner;
+
+class Demo{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("enter the row : ");
+        int row =sc.nextInt();
+        System.out.println("enter the col");
+        int col = sc.nextInt();
+
+        int num[][] = new int[row][col];
+
+        //input
+        for(int i =0; i<row; i++){
+            //colums
+            for(int j =0; j< col; j++){
+                num[i][j] =sc.nextInt();
+            }
+        }
+
+        //output
+        System.out.println("the 2d array is : ");
+         for(int i =0; i<row; i++){
+            //colums
+            for(int j =0; j< col; j++){
+                System.out.print(num[i][j] + " ");
+            }
+            System.out.println();
+        }
+
+
+
+    }
+
+}
