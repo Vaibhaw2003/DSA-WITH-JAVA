8236dd4076dc9548eb735e3cfaa2667785855871
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-17 11:21:06 +0530

    FUNCTION OF FACTORIAL

--- FUNCTIONS/Demo.java
+++ FUNCTIONS/Demo.java
@@ -40,20 +40,43 @@
 
 //---------- FIND FACTORIAL OF NO. FUNCTIONS IN JAVA ----------
 
-import java.util.Scanner;
-class Demo{
-    public static void fact(int n){
-        int factorial = 1;
-        for(int i= n; i> 1; i--){
-            factorial = factorial *i;
-        }
-        System.out.println(factorial);
-    }
+// import java.util.Scanner;
+// class Demo{
+//     public static void fact(int n){
+//         int factorial = 1;
+//         for(int i= n; i> 1; i--){
+//             factorial = factorial *i;
+//         }
+//         System.out.println(factorial);
+//     }
 
-    public static void main(String[] args){
-        Scanner sc = new Scanner(System.in);
-        int n = sc.nextInt();
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the numner : ");
+//         int n = sc.nextInt();
 
-        fact(n);
-    }
-}
+//         fact(n);
+//     }
+// }
+
+//-------- TO FIND THE  FACTORIAL OF N NUMBER ---------
+
+// import java.util.Scanner;
+
+// class Demo{
+
+//     public static void fact(int n){
+//         int fc = 1;
+//         for(int i = n; i>0; i--){
+//              fc =fc *i; 
+//         }
+//         System.out.println(fc); 
+//     }
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the numbr : ");
+//         int n = sc.nextInt();
+
+//         fact(n);
+//     }
+// }
