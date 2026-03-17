9ac25369886bdde0b5cb703bde2b9fad00b59638
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-17 11:00:07 +0530

    2D ARRAY DONE

--- 2D-ARRAY/Demo.java
+++ 2D-ARRAY/Demo.java
@@ -34,34 +34,67 @@
 
 //------- TO PRINT THE 3*3 MATRIX ---------
 
-import java.util.Scanner;
+// import java.util.Scanner;
 
-class Demo{
-    public static void main(String[] args) {
+// class Demo{
+//     public static void main(String[] args) {
 
-        Scanner sc = new Scanner(System.in);
-         System.out.println("enter the row : ");
-         int row =sc.nextInt();
-         System.out.println("enter the col");
-         int col = sc.nextInt();
+//         Scanner sc = new Scanner(System.in);
+//          System.out.println("enter the row : ");
+//          int row =sc.nextInt();
+//          System.out.println("enter the col");
+//          int col = sc.nextInt();
 
-         int num[][] = new int[row][col];
+//          int num[][] = new int[row][col];
 
-         // input 
-         for(int i =0; i<row; i++){
-            for(int j =0; j<col; j++ ){
-                num[i][j] =sc.nextInt();
-            }
-         }
+//          // input 
+//          for(int i =0; i<row; i++){
+//             for(int j =0; j<col; j++ ){
+//                 num[i][j] =sc.nextInt();
+//             }
+//          }
 
-         //output 
-         System.out.println("the output is here");
-         for(int i = 0; i< row; i++){
-            for(int j =0; j< col; j++){
-                System.out.print(num[i][j] + " ");
-            }
-            System.out.println();
-         }
+//          //output 
+//          System.out.println("the output is here");
+//          for(int i = 0; i< row; i++){
+//             for(int j =0; j< col; j++){
+//                 System.out.print(num[i][j] + " ");
+//             }
+//             System.out.println();
+//          }
         
-    }
-}
+//     }
+// }
+
+//----------- TO PRINT THE MTRIX 4*4 ------------
+
+// import java.util.Scanner;
+
+// class Demo{
+//     public static void main(String[] args) {
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the size of row : ");
+//         int row = sc.nextInt();
+//         System.out.println("enter the size of col :");
+//         int col = sc.nextInt();
+
+//         int size[][] = new int[row][col];
+
+//         //input
+//         for(int i = 0; i<row; i++){
+//             for(int j =0; j< col; j++){
+//               size[i][j]=  sc.nextInt();
+//             }
+//         }
+
+//         //output
+//         System.out.println("the matrix is : ");
+//          for(int i = 0; i<row; i++){
+//             for(int j =0; j< col; j++){
+//               System.out.print(size[i][j] + " ");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
