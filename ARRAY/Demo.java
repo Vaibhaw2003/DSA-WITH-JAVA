aa7fafba2013de399318748efc619524d7661fd5
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-17 11:01:21 +0530

    ARRAY DONE

--- ARRAY/Demo.java
+++ ARRAY/Demo.java
@@ -206,3 +206,38 @@
 // System.out.println(result);
 //      }
 // }
+
+//--------- SELECTION SORT ---------
+
+// class Demo {
+//     public static void main(String[] args) {
+
+//         int arr[] = {64, 25, 12, 22, 11};
+//         int n = arr.length;
+
+//         for(int i = 0; i < n - 1; i++) {
+
+//             int minIndex = i;
+
+//             for(int j = i + 1; j < n; j++) {
+//                 if(arr[j] < arr[minIndex]) {
+//                     minIndex = j;
+//                 }
+//             }
+
+//             int temp = arr[minIndex];
+//             arr[minIndex] = arr[i];
+//             arr[i] = temp;
+//         }
+
+//         System.out.println("Sorted Array:");
+
+//         for(int i = 0; i < n; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
+
+//----------- INSERTION SORT --------
+
+
