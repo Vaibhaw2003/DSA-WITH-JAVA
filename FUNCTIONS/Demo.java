b4cd36fd339374fef7d42d7b1690f2b4a8eb56f5
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-03-26 10:12:53 +0530

    THE HOLLOW SQUARE

--- FUNCTIONS/Demo.java
+++ FUNCTIONS/Demo.java
@@ -64,7 +64,6 @@
 // import java.util.Scanner;
 
 // class Demo{
-
 //     public static void fact(int n){
 //         int fc = 1;
 //         for(int i = n; i>0; i--){
@@ -80,3 +79,25 @@
 //         fact(n);
 //     }
 // }
+
+
+//--------- TO PRINT THE HOLLOW SQUARE PATTERN ---------
+
+class Demo {
+    public static void main(String[] args) {
+        int n = 6;
+
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < n; j++) {
+
+                // Border condition
+                if (i == 0 || i == n - 1 || j == 0 || j == n - 1) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+}
