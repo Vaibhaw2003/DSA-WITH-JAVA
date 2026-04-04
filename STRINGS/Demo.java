5d8ac100bca91347fead6d67fd34049fb92b6b49
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-04-04 23:44:04 +0530

    TO CHECK ANAGRAM

--- STRINGS/Demo.java
+++ STRINGS/Demo.java
@@ -224,3 +224,21 @@
             
 //         }
 //     }
+
+//---------- TO CHECK ANAGRAM ---------
+
+import java.util.Arrays;
+class Demo{
+    public static void main(String[] args) {
+        String a = "listen";
+        String b = "silent";
+
+        char[] c1 = a.toCharArray();
+        char[] c2 = b.toCharArray();
+
+        Arrays.sort(c1);
+        Arrays.sort(c2);
+
+        System.out.println(Arrays.equals(c1, c2));
+    }
+}
