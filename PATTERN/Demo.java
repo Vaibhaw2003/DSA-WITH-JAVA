6105703632b9fdb0443b5d3837a961e9a91e0811
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-04-16 23:34:50 +0530

    THE HOLLOW SQUARE PATTERN

--- PATTERN/Demo.java
+++ PATTERN/Demo.java
@@ -70,3 +70,21 @@
 //         }
 //     }
 // }
+
+
+//------- TO PRINT THE HOLLOW SQUARE PATTERN ---------
+
+// import java.util.Scanner;
+// class Demo{
+//     public static void main(String[] args) {
+//         int i,j,n;
+//         Scanner sc = new Scanner(System.in);
+//         System.out.println("enter the number: ");
+//         n = sc.nextInt();
+//         for(i = 1; i<=n ; i++){        
+//             for(j =1; j<= n; j++){
+//                 System.out.print("*" + " ");
+//             }System.out.println();
+//         }
+//     }
+// }
