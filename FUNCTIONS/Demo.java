5d4eb51682814c6c72fb186ffbd9c5b083bfad41
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-05-01 12:34:01 +0530

    PRINT THE AVERAGE NUMBER

--- FUNCTIONS/Demo.java
+++ FUNCTIONS/Demo.java
@@ -209,3 +209,17 @@
 //         prime(num);
 //     }
 // }
+
+//-------- TO PRINT THE AVERAGE NUMBER --------
+
+class Demo{
+    public void avg(int a ,int b){
+        int avg = (a + b) / 2;
+        System.out.println(avg);
+    }
+    public static void main(String[] args) {
+        Demo d = new Demo();
+        d.avg(10, 20);
+
+    }
+}
