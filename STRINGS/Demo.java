dc72e8226630df0f92216165e985795e2dd4059f
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-05-22 23:54:01 +0530

    TO REVERSE THE STRING

--- STRINGS/Demo.java
+++ STRINGS/Demo.java
@@ -469,3 +469,20 @@
 //                         System.out.println("both string are not equal");
 //         }
 // }
+
+
+//--------- TO REVERSE THE STRING ---------
+
+class Demo {
+        public static void main(String[] args) {
+                String name = "vaibhaw singh";
+                String rev = " ";
+
+                for(int i = name.length()-1; i>=0; i--){
+                        rev = rev + name.charAt(i);
+                }
+                System.out.println(rev);
+        }
+
+        
+}
