f73916e68f263cc929e41cc7dd36b16d7354f294
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-05-28 01:40:54 +0530

    Algorithm to solve 4-queens problem.

--- RECURSION/Demo.java
+++ RECURSION/Demo.java
@@ -200,5 +200,79 @@
 //             int ans = s.fib(5);
 //             System.out.println(ans);
 //         }
-    
 //  }
+
+
+//------- Write an algorithm to solve 4-queens problem. ----------
+
+// class Demo {
+
+//     static int n = 4;
+
+//     static boolean isSafe(int board[][], int row, int col) {
+
+//         // Check left side of row
+//         for (int i = 0; i < col; i++) {
+//             if (board[row][i] == 1)
+//                 return false;
+//         }
+
+//         // Check upper diagonal
+//         for (int i = row, j = col; i >= 0 && j >= 0; i--, j--) {
+//             if (board[i][j] == 1)
+//                 return false;
+//         }
+
+//         // Check lower diagonal
+//         for (int i = row, j = col; i < n && j >= 0; i++, j--) {
+//             if (board[i][j] == 1)
+//                 return false;
+//         }
+
+//         return true;
+//     }
+
+//     static boolean solveNQUtil(int board[][], int col) {
+
+//         if (col >= n)
+//             return true;
+
+//         for (int i = 0; i < n; i++) {
+
+//             if (isSafe(board, i, col)) {
+
+//                 board[i][col] = 1;
+
+//                 if (solveNQUtil(board, col + 1))
+//                     return true;
+
+//                 board[i][col] = 0;
+//             }
+//         }
+
+//         return false;
+//     }
+
+//     static void printSolution(int board[][]) {
+
+//         for (int i = 0; i < n; i++) {
+
+//             for (int j = 0; j < n; j++)
+//                 System.out.print(board[i][j] + " ");
+
+//             System.out.println();
+//         }
+//     }
+
+//     public static void main(String[] args) {
+
+//         int board[][] = new int[n][n];
+
+//         if (solveNQUtil(board, 0) == false) {
+//             System.out.println("Solution does not exist");
+//             return;
+//         }
+
+//         printSolution(board);
+//     }
+// }
