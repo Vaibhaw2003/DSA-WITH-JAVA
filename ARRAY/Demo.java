f4d617a67f84446deae7e52e78a83080054d2554
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-06-02 23:34:20 +0530

    BUBBLE SORT

--- ARRAY/Demo.java
+++ ARRAY/Demo.java
@@ -240,25 +240,47 @@
 
 //----------- INSERTION SORT --------
 
-class Demo{
-    public static void main(String[] args){
-        int arr[] = {1,2,3,4,4,5,5,6};
-        int n = arr.length;
-        for(int i = 1; i < n; i++) {
-            int key = arr[i];
-            int j = i - 1;
+// class Demo{
+//     public static void main(String[] args){
+//         int arr[] = {1,2,3,4,4,5,5,6};
+//         int n = arr.length;
+//         for(int i = 1; i < n; i++) {
+//             int key = arr[i];
+//             int j = i - 1;
 
-            while(j >= 0 && arr[j] > key) {
-                arr[j + 1] = arr[j];
-                j--;
-            }
-            arr[j + 1] = key;
-        }
-        System.out.println("Sorted Array:");
-        for(int i = 0; i < n; i++) {
-            System.out.print(arr[i] + " ");
-        }
-    }
-}
+//             while(j >= 0 && arr[j] > key) {
+//                 arr[j + 1] = arr[j];
+//                 j--;
+//             }
+//             arr[j + 1] = key;
+//         }
+//         System.out.println("Sorted Array:");
+//         for(int i = 0; i < n; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
+
+//---------- BUBBLE SORT ---------
+
+// class Demo{
+//     public static void main(String[] args) {
+//         int arr[] = {1,2,3,4,5,6};
+//         int n = arr.length;
+//         for(int i = 0; i < n - 1; i++) {
+//             for(int j = 0; j < n - i - 1; j++) {
+//                 if(arr[j] > arr[j + 1]) {
+//                     int temp = arr[j];
+//                     arr[j] = arr[j + 1];
+//                     arr[j + 1] = temp;
+//                 }
+//             }
+//         }
+//         System.out.println("Sorted Array:");
+//         for(int i = 0; i < n; i++) {
+//             System.out.print(arr[i] + " ");
+//         }
+//     }
+// }
 
 
