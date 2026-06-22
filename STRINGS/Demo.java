e6cec138b86bcf58c8defa1ebfec103901283851
vaibhawrajput05@gmail.com <vaibhawrajput05@gmail.com>
2026-06-22 15:19:26 +0530

    REVERSE THE STRING given from the user

--- STRINGS/Demo.java
+++ STRINGS/Demo.java
@@ -531,3 +531,21 @@
 //         }
 // }
 
+import java.util.Scanner;
+
+class Demo{
+        public static void main(String[] args) {
+                System.out.println("enter the name");
+                Scanner sc = new Scanner(System.in);
+                String name = sc.nextLine();
+
+                String rev =" ";
+
+                for(int i =name.length()-1; i>=0; i--){
+                        rev = rev + name.charAt(i);
+
+                }
+                System.out.println(rev);
+        }
+}
+
