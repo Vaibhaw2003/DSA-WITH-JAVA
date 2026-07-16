ec6a23b3c204f700c48b250b18993c8003abe2ec
VAIBHAW SINGH <vaibhawrajput05@gmail.com>
2026-07-16 23:40:10 +0530

    Update Demo.java

--- TWO-POINTER/Demo.java
+++ TWO-POINTER/Demo.java
@@ -8,7 +8,6 @@
 
 //         for(int i = 0; i<arr.length -1; i++){
 //             for(int j = 0; j< arr.length-1; j++){
-
 //                 if (arr[j]>arr[j+1]) {
 //                    // swap
 //                     int temp = arr[j];
