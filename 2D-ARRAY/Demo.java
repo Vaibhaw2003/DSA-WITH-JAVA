a1e232c14aa5fed7d6d730bbbc70d6f81580d68b
VAIBHAW SINGH <vaibhawrajput05@gmail.com>
2026-07-24 22:50:51 +0530

    Update Demo.java

--- 2D-ARRAY/Demo.java
+++ 2D-ARRAY/Demo.java
@@ -40,9 +40,7 @@
 //          int row =sc.nextInt();
 //          System.out.println("enter the col");
 //          int col = sc.nextInt();
-
 //          int num[][] = new int[row][col];
-
 //          // input 
 //          for(int i =0; i<row; i++){
 //             for(int j =0; j<col; j++ ){
